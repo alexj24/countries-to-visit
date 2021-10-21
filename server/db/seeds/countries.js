74c4b430096469d38fd35f30725abf94b891252b
alexj24 <alexjj2410@gmail.com>
2021-10-21 13:45:00 +1300

    Add aus to visited seeds

--- server/db/seeds/countries.js
+++ server/db/seeds/countries.js
@@ -7,7 +7,8 @@ exports.seed = function(knex) {
       return knex('countries').insert([
         {id: 1, name: 'Sweden', flag: 'https://flagcdn.com/w320/se.png', continent: 'Northern Europe', capital: 'Oslo'},
         {id: 2, name: 'United States', flag: 'https://flagcdn.com/w320/us.png', continent: 'North America', capital: "Washington DC"},
-        {id: 3, name: 'Pakistan', flag: 'https://flagcdn.com/w320/pk.png', continent: 'Southern Asia', capital: "Islamabad"}
+        {id: 3, name: 'Pakistan', flag: 'https://flagcdn.com/w320/pk.png', continent: 'Southern Asia', capital: "Islamabad"},
+        {id: 4, name: 'Australia', flag: 'https://flagcdn.com/w320/au.png', continent: 'Oceania', capital: "Canberra", visited: true}
       ]);
     });
 };
